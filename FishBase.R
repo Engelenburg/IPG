0ade436b3b0b2a37c1f8e7ab5d62a4b5c73cb15a
Engelenburg <mar_tijn_angelus@hotmail.com>
2018-10-11 13:07:03 +0200

    Update FishBase.R

--- FishBase.R
+++ FishBase.R
@@ -1,8 +1,25 @@
 library(rfishbase)
-source("GBIF_Belgium.R")
-s <- AllData$species
-Species <- unique(Species)
+#source("GBIF_Belgium.R")
+#Reading in the file that has the fish occurance data in Belgium 
+AllDataFishData = read.csv("D:/Documenten/Data IPG/FIsh_Belgium.csv", header=TRUE,sep="\t", dec = ".")
 
-morpho <- morphology(species_list = Species)
-eat <-diet(species_list = Species)
+Species <- AllData$species
+UniqueBelgium <- unique(Species)
 
+UniqueBelgiumString=c()
+for (i in UniqueBelgium){
+  str(i)
+  UniqueBelgiumString=c(UniqueBelgiumString,i)
+}
+
+morpho <- morphology (species_list = UniqueBelgiumString)
+
+eat <-diet(species_list = UniqueBelgiumString)
+
+HabitatEcology <- ecology (species_list = UniqueBelgiumString)
+HabitatEcosystem <- ecosystem(species_list = UniqueBelgiumString)
+
+
+FrequencyofLength <- length_freq(species_list = UniqueBelgiumString)
+Length <- length_length(species_list = UniqueBelgiumString)
+Weigth <- length_weight(species_list = UniqueBelgiumString)
